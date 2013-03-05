1e8c8376f88d32a4b2b2ce7fd94c23b31b087aeb
Oliver Hoff <oliver@hofff.com>
2013-03-05 22:10:18 +0000

    add autoload files

---
+++ TL_ROOT/system/modules/backboneit_image/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package Backboneit_image
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'Image' => 'system/modules/backboneit_image/Image.php',
+));
